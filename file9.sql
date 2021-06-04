fb73390356cde480196af695dc9319b6a43a9689
Ever Ortiz <eortiz@posibillian.tech>
2021-06-04 17:33:39 -0400

    add new file

---
+++ file9.sql
@@ -0,0 +1,13 @@
+-- file5
+
+
+-- DROP TABLE `myTable`;
+
+-- CREATE TABLE `myTable` (
+--   `id` mediumint(8) unsigned NOT NULL auto_increment,
+--   `colum_1` varchar(255) default NULL,
+--   `colum_2` varchar(100) default NULL,
+--   `colum_3` varchar(255),
+--   `colum_4` varchar(13) default NULL,
+--   PRIMARY KEY (`id`)
+-- ) AUTO_INCREMENT=1;
